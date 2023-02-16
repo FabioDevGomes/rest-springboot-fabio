aeec7c030e1a589faebc5b6f775df96846bb67e2
Fábio Dev <fabio.dev.gomes@gmail.com>
2023-02-16 13:16:26 -0300

    [r1.0] - service layer for sum math operation

--- first-java-springboot/rest-springboot-fabio-1/src/main/java/br/com/fabio/MathController.java
+++ first-java-springboot/rest-springboot-fabio-1/src/main/java/br/com/fabio/MathController.java
@@ -1,5 +1,7 @@
 package br.com.fabio;
 
+import java.util.Objects;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
 	@GetMapping(value = "sum/{numberOne}/{numberTwo}")
-	public Double greating(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+	public Double greating(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 		
-		return 1D;
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if(Objects.isNull(strNumber)) {
+			return 0D;
+		}
+		
+		String number = strNumber.replace(",", ".");
+		if(isNumeric(strNumber)) {
+			return Double.parseDouble(strNumber);
+		}
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if(Objects.isNull(strNumber)) {
+			return false;
+		}
+		String number = strNumber.replace(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 }
