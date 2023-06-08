601590e6992c40f22a64ad329c87cda085aa94e7
Fábio Dev <fabio.dev.gomes@gmail.com>
2023-06-07 21:51:58 -0300

    [r1.0] - add swaager details for getbById endpoint

--- src/main/java/br/com/fabio/controller/PersonController.java
+++ src/main/java/br/com/fabio/controller/PersonController.java
@@ -18,15 +18,34 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.fabio.dto.vo.PersonDto;
 import br.com.fabio.dto.vo.v2.PersonDtoV2;
 import br.com.fabio.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/person")
+@Tag(name = "Person", description = "Endpoints for manager people")
 public class PersonController {
 	
 	@Autowired
 	PersonService personService;
 	
 	@GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@Operation(summary = "Find a person - sumary", description = "Find a person - description", responses = {
+			@ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = PersonDto.class)),
+					@Content(mediaType = "application/xml", schema = @Schema(implementation = PersonDto.class))
+				}
+			),
+			@ApiResponse(responseCode = "400", content = @Content),
+			@ApiResponse(responseCode = "401", content = @Content),
+			@ApiResponse(responseCode = "404", content = @Content),
+			@ApiResponse(responseCode = "500", content = @Content)
+		}
+	)
 	public PersonDto findById(@PathVariable(value = "id") Long id) {
 		return personService.findById(id);
 	}
